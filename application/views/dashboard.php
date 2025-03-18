64411bf1f93b5890f2c2e6875a0694fac36e852d
Muhammad Tio Laksono <33713735+ujangproject@users.noreply.github.com>
2025-03-18 16:58:25 +0700

    up menu for mtc

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,12 +1,13 @@
 <h2 style="font-weight: normal;">Welcome to Departments Inventory Online</h2>
 <div class="push">
     <ol class="breadcrumb">
-        <li><i class='fa fa-home'></i>                                                                                                                                                                                                                                                                                                                                                                                             <?php echo anchor('dashboard', "Dashboard"); ?></li>
+        <li><i class='fa fa-home'></i>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               <?php echo anchor('dashboard', "Dashboard"); ?></li>
     </ol>
 </div>
 
 <?php
     $level = $this->session->userdata('level');
+    $dept  = $this->session->userdata('dept_code');
 
 if ($level == 1) {?>
     <div class="col-sm-3">
@@ -46,6 +47,33 @@ if ($level == 1) {?>
             </tr>
             <tr>
                 <th><?php echo anchor('dimp', 'Masuk', ['class' => 'btn btn-danger']); ?></th>
+            </tr>
+        </table>
+    </div>
+<?php } elseif ($dept == 'MTC') {?>
+    <div class="col-sm-4">
+        <table class="table table-bordered">
+            <tr class="success">
+                <th>Departments Device</th>
+            </tr>
+            <tr>
+                <th>Daftar Kartu Riwayat Mesin MTC <br> &nbsp;</th>
+            </tr>
+            <tr>
+                <th><a href="<?php echo base_url('kartu_riwayat/index_NOW'); ?>" class="btn btn-primary">MASUK</a></th>
+            </tr>
+        </table>
+    </div>
+    <div class="col-sm-4">
+        <table class="table table-bordered">
+            <tr class="success">
+                <th>Forms</th>
+            </tr>
+            <tr>
+                <th>Kartu Stock MTC <br> &nbsp; </th>
+            </tr>
+            <tr>
+                <th><a href="<?php echo base_url('kartu_riwayat/kartu_stock'); ?>" class="btn btn-success">MASUK</a></th>
             </tr>
         </table>
     </div>
